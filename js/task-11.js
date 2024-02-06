3c8418d36c619b8dca45dc389ce96b035b8c1989
Nastya <nastya.syasyafi@gmail.com>
2024-02-07 00:09:24 +0200

    Create task-11.js

---
+++ js/task-11.js
@@ -0,0 +1,15 @@
+let a = 5;
+a += 2;
+console.log(a); // 7
+let b = 10;
+console.log(b); // 6
+let c = 15;
+console.log(c); // 45
+let d = 20;
+console.log(d); // 2
+
+// Change code below this line
+a += 2;
+b -= 4;
+c *= 3;
+d /= 10;
